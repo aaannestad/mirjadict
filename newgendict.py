3e90a795cb5f87a79adbd2f2da828b55d6344c26
aaannestad <a.aannestad@gmail.com>
2023-10-17 17:11:04 -0500

    Replacement function actually working

--- newgendict.py
+++ newgendict.py
@@ -15,7 +15,7 @@ teststring = 'OBJ, thing, other thing, SUBJ'
 sortdict = sorted(dictlist, key = lambda x: x['form'])
 
 #find and replace defined strings with LaTeX commands; mapping is found in commandreplace.yaml
-def findandreplace(d):
+def findandreplace(d, rlist):
 
     def replaceall(string, replacedict):
 
@@ -36,11 +36,11 @@ def findandreplace(d):
 
     for k, v in kvps:
         if isinstance(v, str):
-            d[k] = replaceall(v)
+            d[k] = replaceall(v, rlist)
         elif isinstance(v, dict) or isinstance(v, list):
-            findandreplace(v)
+            findandreplace(v, rlist)
 
-findandreplace(sortdict)
+findandreplace(sortdict, replacelist)
 
 for item in sortdict:
     print(item['form'])
