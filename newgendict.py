9721c5a41b845d050f18b69d8649eb2d7fff4fcd
aaannestad <a.aannestad@gmail.com>
2023-10-17 16:57:40 -0500

    Replacement function working

--- newgendict.py
+++ newgendict.py
@@ -9,8 +9,19 @@ with open('newdict.yaml') as dictfile:
 with open('commandreplace.yaml') as replacefile:
     replacelist = yaml.safe_load(replacefile)
 
-for item in replacelist:
-    print(item)
+teststring = 'OBJ, thing, other thing, SUBJ'
+
+def replaceone(string, replacesource, replacetarget):
+    altereds = string.replace(replacesource, replacetarget)
+    return altereds
+
+def replaceall(string, replacedict):
+    altereds = string
+    for input, output in replacedict.items():
+        altereds = altereds.replace(input, output)
+    print(altereds)
+
+replaceall(teststring, replacelist)
 
 #sort dictionary
 sortdict = sorted(dictlist, key = lambda x: x['form'])
